d0ad67bbcd770236f2d7a9cd1ee071e3ee9fac86
Leander Lindahl <leanderlindahl@gmail.com>
2019-11-30 11:22:36 +0100

    Todo App stage 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,58 @@
 import React from 'react';
-import { NextPage } from 'next';
+import { render } from 'react-dom';
 
-interface InitialProps {
-  greeting: string;
+interface Task {
+  title: string;
 }
 
-interface Props extends InitialProps {}
-
-const IndexPage: NextPage<Props, InitialProps> = (props) => {
-  return <div>{props.greeting}</div>
+interface TasksListProps {
+  initialTasks: Task[];
+}
+interface TasksListState {
+  tasks: Task[];
 }
 
-IndexPage.getInitialProps = async () => ({
-  greeting: 'Hello World!'
-});
+class TasksList extends React.Component<TasksListProps, TasksListState> {
+  constructor(props: TasksListProps) {
+    super(props);
+    this.state = {
+      tasks: props.initialTasks
+    };
+    this.onAddNewTaskClick = this.onAddNewTaskClick.bind(this);
+  }
 
-export default IndexPage;
+  onAddNewTaskClick() {
+    this.setState({
+      tasks: [
+        ...this.state.tasks,
+        {
+          title: 'New Task'
+        }
+      ]
+    });
+  }
+
+  render() {
+    const { tasks } = this.state;
+    return (
+      <ul>
+        {tasks.map((task, i) => {
+          return <li key={i}>{task.title}</li>;
+        })}
+        <button onClick={this.onAddNewTaskClick}>Add new task</button>
+      </ul>
+    );
+  }
+}
+
+const tasks: Task[] = [
+  { title: 'Task One' },
+  { title: 'Task Two' },
+  { title: 'Task Three' }
+];
+
+export default () => (
+  <div>
+    <TasksList initialTasks={tasks} />
+  </div>
+);
