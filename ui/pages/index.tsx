7b0a58b36e92863f3ad40c6078f00cdb41f0abd9
Leander Lindahl <leanderlindahl@gmail.com>
2019-12-02 16:23:17 +0100

    Add graphql-codegen

--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { TasksComponent, TaskStatus } from '../generated/graphql';
 
 interface InitialProps {
   greeting: string;
@@ -8,7 +9,21 @@ interface InitialProps {
 interface Props extends InitialProps {}
 
 const IndexPage: NextPage<Props, InitialProps> = (props) => {
-  return <div>{props.greeting}</div>
+  return <TasksComponent variables={{status: TaskStatus.Active}}>{({loading, error, data}) => {
+    if (loading) {
+      return <p>Loading.</p>
+    } else if (error) {
+      return <p>An error occured. {console.log(error)}</p>
+    }
+    const tasks = data && data.tasks ? data.tasks : [];
+    return (
+      <ul>{
+        tasks.map(task => {
+          return <li key={task.id}>{task.title}</li>
+        })}
+      </ul>
+    );
+  }}</TasksComponent>
 }
 
 IndexPage.getInitialProps = async () => ({
